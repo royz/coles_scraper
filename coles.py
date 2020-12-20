59d2af618be9a69a71e9b1f8d29d470d73084f3e
Rajorshi Roy <35338951+royz@users.noreply.github.com>
2020-12-21 02:54:59 +0530

    added search store function

--- coles.py
+++ coles.py
@@ -1,14 +1,18 @@
 import asyncio
+import csv
+import json
 import time
-
+from pprint import pprint
+import os
 import aiohttp
+import requests
 
 
 class Coles:
 
     def __init__(self):
         self.subscription_key = '2905142349a44ee3a8a418201d1535c4'
-        self.headers = headers = {
+        self.headers = {
             'Ocp-Apim-Subscription-Key': self.subscription_key,
             'X-App-Platform': 'Android',
             'X-OS-Version': '6.0',
@@ -34,8 +38,9 @@ class Coles:
                 # print(status_code)
                 return json_response['results'][0]['text']
         except Exception as e:
-            print(f'failed to get search result')
-            err(e)
+            # print(f'failed to get search result')
+            # err(e)
+            pass
 
     async def _get_all_suggestions(self, items):
         async with aiohttp.ClientSession(headers=self.headers) as session:
@@ -47,30 +52,73 @@ class Coles:
         suggestions = asyncio.run(self._get_all_suggestions(items))
         return suggestions
 
-    async def get_location(self, item_name, store_id):
+    @staticmethod
+    async def get_aisle(session, item_name, store_id):
         params = {
-            'searchTerm': 'sugar',
-            'storeId': '406',
+            'searchTerm': item_name,
+            'storeId': store_id,
             'start': '0',
             'limit': '20',
             'searchToggle': '1',
-            'latitude': '-34.956463',
-            'longitude': '138.565066',
-            'distance': '10.0',
         }
 
-        url = 'https://apigw.coles.com.au/digital/colesappbff/v1/products/search/suggestions'
+        url = 'https://apigw.coles.com.au/digital/colesappbff/v1/products/search'
         try:
             async with session.get(url, params=params) as response:
                 json_response, status_code = await response.json(), response.status
                 # print(status_code)
-                return json_response['results'][0]['text']
+                return item_name, json_response['Results'][0]['Locations'][0]['Aisle']
         except Exception as e:
-            print(f'failed to get search result')
+            print(f'failed to get aisle for {item_name}')
             err(e)
 
-    def get_all_aisles(self):
-        pass
+    async def _get_all_aisles(self, items):
+        async with aiohttp.ClientSession(headers=self.headers) as session:
+            tasks = [coles.get_aisle(session, item, '406') for item in items]
+            results = await asyncio.gather(*tasks)
+            return [result for result in results if result]
+
+    def get_all_aisles(self, items):
+        aisles = asyncio.run(self._get_all_aisles(items))
+        return aisles
+
+    @staticmethod
+    def search_stores():
+        duplicates = []
+        if not os.path.exists(file('input', 'coles_stores_coordinates.csv')):
+            print('coles_stores_coordinates.csv not found')
+            quit()
+        with open(file('input', 'coles_stores_coordinates.csv')) as f:
+            reader = csv.reader(f)
+            coordinates = []
+            for row in reader:
+                if ''.join(row) in duplicates:
+                    pass
+                else:
+                    duplicates.append(''.join(row))
+                    coordinates.append(row)
+
+            stores = {}
+            for i, coordinate in enumerate(coordinates):
+                print(f'[{i + 1} / {len(coordinates)}] {coordinate}:', end=' ')
+                params = {
+                    'latitude': float(coordinate[0]),
+                    'longitude': float(coordinate[1]),
+                    'brandIds': '2,1',
+                    'numberOfStores': 15,
+                }
+                response = requests.get('https://apigw.coles.com.au/digital/colesweb/v1/stores/search', params=params)
+                new_stores = response.json()['stores']
+                new_stores = {new_store['storeId']: new_store for new_store in new_stores}
+                previous_store_count = len(stores)
+                stores.update(new_stores)
+                print(f'{len(new_stores)} stores found. {len(stores) - previous_store_count} new.')
+        with open(file('cache', 'stores.json'), 'w', encoding='utf-8') as f:
+            json.dump(stores, f, indent=2)
+
+
+def file(*filepath):
+    return os.path.join(os.path.dirname(__file__), *filepath)
 
 
 def err(er):
@@ -80,9 +128,5 @@ def err(er):
 
 
 if __name__ == '__main__':
-    items = ['milk', 'biscuit', 'sugar'] * 50
     coles = Coles()
-    start = time.time()
-    suggestions = coles.get_all_suggestions(items)
-    print(suggestions)
-    print(f'time taken: {time.time() - start} sec')
+    coles.search_stores()
